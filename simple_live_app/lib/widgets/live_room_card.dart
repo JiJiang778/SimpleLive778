1f2086c1ca306c4518a7cf9a477abc4b7faf67db
jiang2345 <2786984352@qq.com>
2025-11-26 09:49:43 +0800

    Update live_room_card.dart

--- simple_live_app/lib/widgets/live_room_card.dart
+++ simple_live_app/lib/widgets/live_room_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:remixicon/remixicon.dart';
 import 'package:simple_live_app/app/app_style.dart';
+import 'package:simple_live_app/app/controller/app_settings_controller.dart';
 import 'package:simple_live_app/app/sites.dart';
 import 'package:simple_live_app/app/utils.dart';
 import 'package:simple_live_app/routes/app_navigation.dart';
