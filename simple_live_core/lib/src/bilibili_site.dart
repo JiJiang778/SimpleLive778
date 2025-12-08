c95abcfd24352be1ec2cc09d7fa904cd8eed90a3
JiJiang778 <jijiang778@126.com>
2025-12-08 21:25:45 +0800

    Update bilibili_site.dart

--- simple_live_core/lib/src/bilibili_site.dart
+++ simple_live_core/lib/src/bilibili_site.dart
@@ -30,7 +30,7 @@ class BiliBiliSite implements LiveSite {
   LiveDanmaku getDanmaku() => BiliBiliDanmaku();
 
   static const String kDefaultUserAgent =
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0";
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0";
   static const String kDefaultReferer = "https://live.bilibili.com/";
 
   String buvid3 = "";
