284ae01fa0ceadc05b88788f4e1f5a311f2227d1
JiJiang778 <jijiang778@126.com>
2025-12-08 16:22:20 +0800

    Update main.dart

--- simple_live_app/lib/main.dart
+++ simple_live_app/lib/main.dart
@@ -142,6 +142,9 @@ Future initServices() async {
   DouyinSite().setAbogusUrlFunction(DouyinSign.getAbogusUrl);
   DouyinSite().setSignatureFunction(DouyinSign.getSignature);
   
+  // 设置斗鱼签名函数
+  DouyuSite().setDouyuSignFunction(DouyuSign.getSign);
+  
   initCoreLog();
 }
 
